4e4e6f5b899a2bf15c2263c0efa1b998623cae27
Artem Bolotov <teeemon@gmail.com>
2023-09-07 19:00:16 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by artembolotov on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
